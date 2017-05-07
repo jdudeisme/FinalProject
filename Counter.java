56be06081edd882a7175902e7600269fefe7efa9
jdudeisme <jdudeisme3000@gmail.com>
2017-05-07 15:34:58 -0400

    Update Counter.java

--- Counter.java
+++ Counter.java
@@ -1,1 +1,31 @@
+public class Counter {
+	private static int wellbeing = 0;
+	private static int hunger = 3;
+	
+	public static void main(String[] args){
+		WellbeingUp();
+		WellbeingDown();
+		HungerDown();		
+	}
 
+public static void WellbeingUp(){
+	wellbeing++;
+
+	System.out.println("Your well being is currently at " + wellbeing + ", Nice Job.");
+	
+}
+
+public static void WellbeingDown(){
+	wellbeing--;
+	
+	System.out.println("Oops, Your well being is currently at " + wellbeing + ", bad move.");
+}
+
+public static void HungerDown(){
+	hunger--;
+	
+	System.out.println("You decided to eat, your hunger level is now " + hunger + ", Mmm Satisfying" );
+}
+		
+
+}
