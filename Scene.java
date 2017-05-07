b4d1e6c356b60f4c3f69a73c5ddc8aa4e9fcec0a
jdudeisme <jdudeisme3000@gmail.com>
2017-05-07 16:18:57 -0400

    Update Scene.java
    1 scene down I'd rather have them typing back "bed" or "breakfast" rather than 1/0 but idk how to do that

--- Scene.java
+++ Scene.java
@@ -1,11 +1,28 @@
+import java.awt.*;
 
-
+import java.util.Scanner;
 public class Scene {
 	
+public static void Morning(){
+		
+	Scanner console = new Scanner(System.in);
 	
-public void Morning(){
+	System.out.println("Good Morning! Do you want to stay in bed or head off to breakfast? (1/0)");
+	int AnswerMorning = console.nextInt();
+	if (AnswerMorning == 1){
+		System.out.println("You get that much needed snooze button beauty rest and you feel better about the day ahead of you");
+	Counter.WellbeingUp();
+	}
 	
-	System.out.println("Good Morning! Do you want to stay in bed or head off to breakfast" );
+	else if (AnswerMorning == 0){
+		System.out.println("You eat a wonderful breakfast, but it comes at a cost of being groggy and cranky because you missed that important snooze button beauty rest");
+		Counter.WellbeingDown();
+		Counter.HungerDown();
+	}
+	else 
+		System.out.println("That was an invalid input");
+		
+	console.close();
 }
 
 public void CS112(){
