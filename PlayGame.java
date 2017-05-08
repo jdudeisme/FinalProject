a41cb29050976e2d55effdc3e58e92609d168fbc
jdudeisme <jdudeisme3000@gmail.com>
2017-05-08 15:20:06 -0400

    Update PlayGame.java

--- PlayGame.java
+++ PlayGame.java
@@ -1,8 +1,8 @@
 
+import java.util.Scanner;
 
 public class PlayGame {
-	public boolean HasStudied;
-	
+	public static boolean DinnerSecond = false;
 	
 	public static void main(String[] args){
 		
@@ -10,6 +10,9 @@ public class PlayGame {
 		Scene.Morning();
 		Scene.CS112();
 		Scene.Lunch();
+		Scene.Study();  
+		Scene.Section();
+			TimeShift();
 		
 	
 	}
@@ -18,5 +21,22 @@ public class PlayGame {
 		
 		System.out.println("Welcome to the Game of Life with J-Lo Suave");
 			
+	}
+	
+	public static void TimeShift(){
+		System.out.println("Do you want to do Dinner before a procrastination session(1), or the procrastination session first(2)?");
+		Scanner console = new Scanner(System.in);
+			int TimeShiftAns = console.nextInt();
+			if  (TimeShiftAns == 1){
+				
+				Scene.Dinner();
+				Scene.Procrastinate();
+				
+			}
+			else{		
+				Scene.Procrastinate();
+				Scene.Dinner();
+			}
+			console.close();	
 	}
 }
